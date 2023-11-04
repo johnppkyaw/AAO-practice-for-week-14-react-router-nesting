3f094b84235d9d3af17f8b23e41f32b5412efea4
Pyae (John) Kyaw <jjpyae@gmail.com>
2023-11-04 09:55:33 -0700

    Practice complete

--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,7 +1,21 @@
-function Movies() {
+import { Switch, Route, NavLink } from "react-router-dom";
+import MovieDetails from "../MovieDetails";
+
+function Movies({ movies }) {
+  console.log(movies);
   return (
-    <div className='comp orange'>
+    <div className="comp orange">
       <h1>Movies Component</h1>
+      <nav>
+        {movies.map((item) => (
+          <NavLink to={`/movies/${item.id}`}>{item.title}</NavLink>
+        ))}
+      </nav>
+      <Switch>
+        <Route path="/movies/:movieId">
+          <MovieDetails movies={movies} />
+        </Route>
+      </Switch>
     </div>
   );
 }
